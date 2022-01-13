0859f890afb473edd1293923cb5a40368ad7ef42
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-14 07:40:26 +0900

    feat: 정률 할인 정책 추가

---
+++ core/src/test/java/hello/core/domain/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package hello.core.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import hello.core.domain.member.Grade;
+import hello.core.domain.member.Member;
+
+public class RateDiscountPolicyTest {
+
+	RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+	@Test
+	@DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+	public void vip_o() throws Exception {
+	    //given
+		Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+	    //when
+		int discount = discountPolicy.discount(member, 10000);
+
+	    //then
+		assertThat(discount).isEqualTo(1000);
+	 }
+
+	 @Test
+	 @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+	 public void vip_x() throws Exception {
+	     //given
+		 Member member = new Member(2L, "memberBasic", Grade.BASIC);
+		 //when
+		 int discount = discountPolicy.discount(member, 10000);
+		 //then
+		 assertThat(discount).isEqualTo(0);
+	  }
+}
