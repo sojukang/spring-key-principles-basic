0add46f5b4f7c8ed7880a0979f2932e8da1bece3
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-01-28 07:24:53 +0900

    feat: scope and Provider

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +15,18 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	// private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	@RequestMapping("log-demo")
 	@ResponseBody //view 를 거치지 않고 문자를 그대로 보낸다
-	public String logDemo(HttpServletRequest request) { //java 에서 정의한 http 요청정보를 받을 수 있다
+	public String logDemo(HttpServletRequest request) throws InterruptedException { //java 에서 정의한 http 요청정보를 받을 수 있다
 		String requestURL = request.getRequestURI().toString();
+		MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에
 		myLogger.setRequestURL(requestURL);
 
 		myLogger.log("controller test");
+		Thread.sleep(1000);
 		logDemoService.logic("testId");
 		return "OK";
 	}
